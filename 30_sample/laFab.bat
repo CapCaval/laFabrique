6e708c8798175e65b712262faa8f21b60e72b4d2
Mik Arber <mik@capcaval.org>
2014-02-14 18:03:14 +0100

    Update laFab.bat
    ; change

--- 30_sample/laFab.bat
+++ 30_sample/laFab.bat
@@ -1,8 +1,5 @@
 @echo off
 
-<<<<<<< HEAD
-java -cp .:./10_bin:./02_lib/ccOutils.jar org.capcaval.ccoutils.lafabrique.LaFabrique %*
-=======
 java -cp .;../10_bin;./10_bin;./02_lib/ccOutils.jar org.capcaval.ccoutils.lafabrique.LaFabrique %*
->>>>>>> 7ca053b8518e9c6519c8daa469f16447a236a453
+
 
